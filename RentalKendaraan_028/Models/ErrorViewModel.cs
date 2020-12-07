7090c165605ad591afcc745a4b4c1bc05e86d2ce
Khumairi Zulhizmarch <khumairiari21@gmail.com>
2020-12-07 23:48:36 +0700

    Add project files.

---
+++ RentalKendaraan_028/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentalKendaraan_028.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
